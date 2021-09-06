028b0786cd1df5bc985716c6be49e759f5c5d049
Zois Pagoulatos <zpagoulatos@hotmail.com>
2021-09-06 18:47:53 +0000

    Fixes for eslint

--- src/milestone-actions.ts
+++ src/milestone-actions.ts
@@ -1,5 +1,5 @@
-import { graphql } from '@octokit/graphql'
 import { context, getOctokit } from '@actions/github'
+import { graphql } from '@octokit/graphql'
 
 const findMilestoneByName = async (
   repoToken: string,
